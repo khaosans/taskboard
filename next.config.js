d9a04c89608060e80bff3a3d9e503c729139e162
SourC <souriya.khaosanga@gmail.com>
2024-10-21 21:09:17 -0700

    Refactor: Update next.config.js to add crypto-browserify fallback

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve('crypto-browserify'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
