e09ed61283b41552359a6528ec2508b5107bc798
SourC <souriya.khaosanga@gmail.com>
2024-10-21 21:07:10 -0700

    Refactor: Update chain route and client layout components

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,13 @@
 module.exports = {
   presets: [
       '@babel/preset-env',
-      '@babel/preset-react', // Ensure this is included
+      '@babel/preset-react',
       '@babel/preset-typescript',
   ],
   plugins: [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-proposal-private-property-in-object',
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     // Add any other plugins you need here
   ],
 };
