b3b2a4bfc600f8f181ac28a45adb4295e4fc1147
SourC <souriya.khaosanga@gmail.com>
2024-10-25 09:33:48 -0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,25 @@
 'use client';
 
 import React from 'react';
-import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from 'next-themes';
 import '@/styles/global.css';
-import { Web3ReactProvider } from '@web3-react/core';
-import { Web3Provider } from '@ethersproject/providers';
 import { Toaster } from 'react-hot-toast';
 import TopBar from 'components/TopBar';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <Web3ReactProvider getLibrary={(provider) => new Web3Provider(provider)}>
-              <Toaster />
-              <TopBar onWalletChange={() => {}} selectedWallet={null} />
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="flex flex-col min-h-screen">
+            <TopBar onWalletChange={() => {}} selectedWallet={null} />
+            <main className="flex-grow">
               {children}
-            </Web3ReactProvider>
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+            </main>
+          </div>
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
