662cccab667b9cdcadfa546721bb215680f62792
SourC <souriya.khaosanga@gmail.com>
2024-10-24 23:49:11 -0700

    Feature/fix v5 (#62)
    * Update next version to 15.0.1
    
    * Refactor ChatbotModal and TopBar components
    
    * Refactor e2e-playwrite.yml and clerk-auth.spec.ts
    
    * Refactor eslint configuration to remove unused extends
    
    * Refactor ClientLayout and TopBar components
    
    * Refactor eslint configuration to remove unused extends
    
    * Refactor import statement in login route to use '@clerk/nextjs/server'
    
    * Refactor ClientLayout and TopBar components
    
    * Refactor import statement in login route to use '@clerk/nextjs/server'

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 const TopBar: React.FC = () => {
     return (
-        <nav className="bg-gray-800 p-4 fixed top-0 left-0 right-0 z-10">
+        <nav className="bg-gray-800 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/" className="text-white text-xl font-bold">
                     TaskFlow
