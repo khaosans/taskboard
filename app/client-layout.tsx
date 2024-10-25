0725c039b21a74d620049b70297f1b2aa5ff3bba
SourC <souriya.khaosanga@gmail.com>
2024-10-25 10:51:03 -0700

    Revert "Feature/fix v5 (#62)" (#66)
    This reverts commit 662cccab667b9cdcadfa546721bb215680f62792.

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { useTheme } from '@/hooks/useTheme'
+import TopBar from '@/components/TopBar'
 import RobotTransformerWallpaper from '@/components/RobotTransformerWallpaper'
 
 interface ClientLayoutProps {
@@ -13,8 +14,9 @@ const ClientLayout: React.FC<ClientLayoutProps> = ({ children }) => {
 
   return (
     <div className={`min-h-screen flex flex-col ${theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
+      <TopBar />
       <RobotTransformerWallpaper />
-      <main className="flex-grow">
+      <main className="flex-grow pb-20 pt-16">
         {children}
       </main>
     </div>
