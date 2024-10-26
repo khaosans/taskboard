c664e99b820beb406beb68761f1820195fce87be
SourC <souriya.khaosanga@gmail.com>
2024-10-25 18:36:26 -0700

    Refactor wallet value check in NotificationsPage and PortfolioPage

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   darkMode: 'class',
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: 'your-border-color-here',
+        // other custom colors...
+      },
+    },
   },
   plugins: [],
 }
