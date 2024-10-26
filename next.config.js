d3538a467c8f72447cdd2a760e807201f996e7c4
SourC <souriya.khaosanga@gmail.com>
2024-10-26 15:16:43 -0700

    Refactor Babel configuration and remove unused plugins

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   experimental: {
     appDir: true,
-  },
-  // Disable custom Babel loader
-  webpack: (config, { isServer, dev }) => {
-    // This will remove the custom Babel loader
-    config.module.rules = config.module.rules.filter(
-      (rule) => !(rule.use && rule.use.loader === 'next-babel-loader')
-    );
-    return config;
   },
 };
 
